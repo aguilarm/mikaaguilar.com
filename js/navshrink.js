28a9e7108f545a31c63b05c90339b755e6c30886
Mika Aguilar <mika.aguilar@gmail.com>
2014-07-02 14:33:08 -0500

    Eureka
    Just said 'ITS GLORIOUS' out loud.
    Onward, to the next slide!

--- js/navshrink.js
+++ js/navshrink.js
@@ -1,7 +1,7 @@
 $('document').ready(function($){
 	var header = $('header');
 	$(window).scroll(function () {
-		if ($(this).scrollTop() > 105) {
+		if ($(this).scrollTop() > 400) {
 			header.addClass("small");
 		} else {
 			header.removeClass("small");
