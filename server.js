e59bf738fdb28ffea567a95c3f85a8a1471c9647
aguilarm <mika.aguilar@gmail.com>
2014-11-07 21:02:03 +0000

    Added better Error page, blog removed while in dev branch
    Blog was still in public directory and accessable even though none of the API was set up.  Facepalm.  Also, shiny new 404 page I made while working on another project.

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
 var express = require('express'),
+    swig = require('swig'),
     app = express();
+
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', (__dirname, 'views'));
 
 app.use(express.static(__dirname + '/public'));
 
-//subpathing 
-//can also be used, saving for later
-//this would handle mikaaguilar.com/blog
-//would only work with static files, so I'll want to
-//use views there
-//app.use('/blog', express.static(__dirname + '/blog'));
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
-app.listen(process.env.PORT || 3000);
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + server.address().port);
+    console.log(process.env.IP);
+    if (app.get('env') === 'development') {
+        console.log('RUNNING IN DEV MODE');
+    }
+});
