af275c1ec018e92b677db67ba234cf0511293efe
aguilarm <mika.aguilar@gmail.com>
2019-01-13 18:58:56 -0600

    refactor(inf): Switch to match upstream chart

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,8 @@
-FROM nginx
+FROM alpine:latest
 
-COPY public /usr/share/nginx/html
+COPY . /mnt/project
+
+RUN cd /mnt && tar -czf project.tar.gz project && rm -rf /mnt/project
+
+# Init container will add mnt to srv where an emptydir volume mount is created
+CMD ["time", "/bin/sh", "-c", "time mv /mnt/project.tar.gz /srv/project.tar.gz && time mkdir -p /srv/project && time tar -xzf /srv/project.tar.gz -C /srv && time rm -rf /srv/project.tar.gz"]
--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -12,11 +12,11 @@ $config_directories = [];
 $config_directories['sync'] = '../config/sync';
 
 $databases['default']['default'] = array (
-  'database' => getenv("MYSQL_DATABASE"),
+  'database' => getenv("MYSQL_DB"),
   'username' => getenv("MYSQL_USER"),
-  'password' => getenv("MYSQL_PASSWORD"),
+  'password' => getenv("MYSQL_PASS"),
   'prefix' => '',
-  'host' => getenv("MYSQL_SERVICE_HOST"),
+  'host' => getenv("MYSQL_HOST"),
   'port' => '3306',
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
   'driver' => 'mysql',
